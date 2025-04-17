6a991cc87ebcf760abd40eaef64cd5ffd8c4b768
student <>
2025-04-17 16:43:38 +0900

    Player作成途中

---
+++ Source/Pachio/Private/Player/PlayerCharacter.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/PlayerCharacter.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Interface/StateBase.h"
+//#include "Player/State/DefaultPlayerState.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	//DefaultPlayerState* DP = NewObject<DefaultPlayerState>();
+	//CurrentState.
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void APlayerCharacter::GenerateState()
+{
+	//StateMap.Add("Defaul",TScriptInterface<IStateBase>(NewObject<DefaultPlayerState>()))
+}
+
---
+++ Source/Pachio/Public/Player/PlayerCharacter.h
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include <GameFramework/SpringArmComponent.h>
+#include <Camera/CameraComponent.h>
+#include "PlayerCharacter.generated.h"
+
+class IStateBase;
+
+UCLASS()
+class PACHIO_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void GenerateState();
+
+private:
+	/** Character用のStaticMesh : Capsule  プレイヤー本体の判定用*/
+	UPROPERTY(VisibleAnywhere, Category = Character, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> Capsule;
+
+	/** Characterの攻撃用のStaticMesh : Box 　(下、踏みつけ攻撃など)*/
+	UPROPERTY(VisibleAnywhere, Category = Character, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> StompAttackBox;
+
+	/** Character用のStaticMesh : Box 　(上、ブロック破壊など)*/
+	UPROPERTY(VisibleAnywhere, Category = Character, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> UpperAttackBox;
+
+	/** Cameraを配置するためのSpringArm */
+	UPROPERTY(VisibleAnywhere, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	/** SpringArmの先端に配置するカメラ */
+	UPROPERTY(VisibleAnywhere, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> Camera;
+
+private:
+	UPROPERTY()
+	TSubclassOf<IStateBase>CurrentState;
+
+	UPROPERTY()
+	TMap<FString, TScriptInterface<IStateBase>> StateMap;
+};
