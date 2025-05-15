aadbe112c409281744b351e81e5ba4562d6820ac
DESKTOP-2S63EIQ\student <>
2025-05-15 16:08:27 +0900

    no message

--- Source/Pachio/Private/Enemy/State/KoopaComponent.cpp
+++ Source/Pachio/Private/Enemy/State/KoopaComponent.cpp
@@ -2,23 +2,39 @@
 
 
 #include "Enemy/State/KoopaComponent.h"
+#include "Enemy/Logic/KoopaAliveState.h"
 
-
-bool UKoopaComponent::OnEnter(AEnemyCharacter* Owner, UWorld*, const FString materialID)
+bool UKoopaComponent::OnEnter(AEnemyCharacter* owner, UWorld* currentLevel, const FString materialID)
 {
-	return false;
+	if(!owner || !currentLevel)
+		return false;
+
+	logic = NewObject<UKoopaAliveState>(this);
+	if (!logic)
+		return false;
+
+	logic->OnEnter(owner, currentLevel, this, materialID);
+
+	return true;
 }
 
 bool UKoopaComponent::OnUpdate(float DeltaTime)
 {
-	return false;
+	if (!logic)
+		return false;
+
+	return logic->OnUpdate(DeltaTime);
 }
 
 bool UKoopaComponent::OnExit()
 {
-	return false;
+	return true;
 }
 
-void UKoopaComponent::OnOverlap(AActor*)
+void UKoopaComponent::OnOverlap(AActor* hitActor)
 {
+	if (!logic)
+		return;
+
+	logic->OnOverlap(hitActor);
 }
