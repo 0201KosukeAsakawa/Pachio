88c8c2cb5b4c7ab9034563064a3bbdab3b0c8ec2
881PC-06\student <>
2025-06-10 12:44:24 +0900

    no message

--- Source/Pachio/Private/Logic/Color/ColorTriggerStopComponent.cpp
+++ Source/Pachio/Private/Logic/Color/ColorTriggerStopComponent.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Logic/Color/ColorTriggerStopComponent.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UColorTriggerStopComponent::UColorTriggerStopComponent()
@@ -11,5 +12,31 @@ UColorTriggerStopComponent::UColorTriggerStopComponent()
 
 void UColorTriggerStopComponent::OnColorMatched()
 {
+    AActor* Owner = Cast<AActor>(GetOwner());
+    if (!Owner)
+        return;
 
+    // --- 当たり判定（Collision）を無効化 ---
+    TArray<UActorComponent*> CollisionComponents = GetOwner()->GetComponentsByClass(UPrimitiveComponent::StaticClass());
+    for (UActorComponent* Comp : CollisionComponents)
+    {
+        UPrimitiveComponent* PrimComp = Cast<UPrimitiveComponent>(Comp);
+        if (PrimComp)
+        {
+            // 衝突無効化
+            PrimComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+        }
+    }
+
+    // --- 見た目（メッシュ等）を非表示に ---
+    TArray<UActorComponent*> MeshComponents = Owner->GetComponentsByClass(UMeshComponent::StaticClass());
+    for (UActorComponent* Comp : MeshComponents)
+    {
+        UMeshComponent* MeshComp = Cast<UMeshComponent>(Comp);
+        if (MeshComp)
+        {
+            MeshComp->SetVisibility(false, true);  // 子も含めて非表示
+            MeshComp->SetHiddenInGame(true);
+        }
+    }
 }
