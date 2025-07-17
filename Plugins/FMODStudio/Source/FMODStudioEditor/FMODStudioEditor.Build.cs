cf064ecfaf91408f8e1ba632bcfd0fe4a1dd6a23
student <>
2025-07-17 14:58:48 +0900

    テスト

--- Plugins/FMODStudio/Source/FMODStudioEditor/FMODStudioEditor.Build.cs
+++ Plugins/FMODStudio/Source/FMODStudioEditor/FMODStudioEditor.Build.cs
@@ -52,6 +52,7 @@ namespace UnrealBuildTool.Rules
                     "MovieSceneTools",
                     "PropertyEditor",
                     "Settings",
+                    "SequencerCore",
                     "Slate",
                     "SlateCore",
                     "Sockets",
