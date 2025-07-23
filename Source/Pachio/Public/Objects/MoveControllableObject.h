39452a652e22d8630c4e63990478896525768bd1
DESKTOP-PVU7SP0\tonto <>
2025-07-23 18:04:14 +0900

    ステージ一個できた

--- Source/Pachio/Public/Objects/MoveControllableObject.h
+++ Source/Pachio/Public/Objects/MoveControllableObject.h
@@ -31,6 +31,7 @@ public:
 	void OnFootBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
 		bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION(BlueprintCallable)
 	void OnFootEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
 		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 private:
